3d3932da09faa56f002f1cd8eee526db398f9691
matias <matiasgonzaloacosta@gmail.com>
2020-09-28 21:14:47 -0300

    first commit

---
+++ docs/precache-manifest.d71e8862eb3c4e16f7cd0097e835b84e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71cf92ae66f5674abdf80539eb71ca7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bd2bea355648b0b800a",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "e13b83fe5f8facbb2b3d",
+    "url": "/static/css/main.4021a525.chunk.css"
+  },
+  {
+    "revision": "9bd2bea355648b0b800a",
+    "url": "/static/js/2.1a68ba2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a68ba2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13b83fe5f8facbb2b3d",
+    "url": "/static/js/main.1ba85d43.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
